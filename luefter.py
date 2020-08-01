97a342ccbd602edfbfccd419ad94c564e9b21f69
phoiniks <phoiniks@grellopolis.de>
2020-08-01 22:14:09 +0200

    Erste Version

---
+++ luefter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from os import remove
+from os.path import exists
+from socket import gethostname
+from subprocess import check_output, run
+from sys import argv
+
+run(["sudo", "/usr/bin/jetson_clocks"])
+
+temperatur = check_output(["cat", "/sys/class/thermal/thermal_zone0/temp"])
+temperatur = "{:.2f}".format(float(temperatur.decode("utf-8")) / 1000)
+pwm = check_output(["cat", "/sys/devices/pwm-fan/target_pwm"])
+pwm = pwm.rstrip()
+
+temperatur_float = float(temperatur)
+
+if temperatur_float < 36.0:
+    run(["sudo", "sh", "-c", "echo 100 > /sys/devices/pwm-fan/target_pwm"])
+if temperatur_float > 36.0 and temperatur_float < 37.0:
+    run(["sudo", "sh",  "-c", "echo 150 > /sys/devices/pwm-fan/target_pwm"])
+if temperatur_float > 37.0 and temperatur_float < 38.0:
+    run(["sudo", "sh",  "-c", "echo 200 > /sys/devices/pwm-fan/target_pwm"])
+if temperatur_float > 38.0:
+    run(["sudo", "sh",  "-c", "echo 255 > /sys/devices/pwm-fan/target_pwm"])
